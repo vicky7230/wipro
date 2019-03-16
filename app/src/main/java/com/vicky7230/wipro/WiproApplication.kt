080339cb0499f4b0216a5c6508a6ccc698886369
Vipin Kumar <vipin@mildlyclassic.com>
2019-03-16 21:09:21 +0530

    initial commit

---
+++ app/src/main/java/com/vicky7230/wipro/WiproApplication.kt
@@ -0,0 +1,37 @@
+package com.vicky7230.wipro
+
+import android.app.Activity
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import timber.log.Timber
+import javax.inject.Inject
+
+class WiproApplication : Application(), HasActivityInjector{
+
+    @Inject
+    lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        /*DaggerApplicationComponent
+            .builder()
+            .application(this)
+            .build()
+            .inject(this)*/
+
+
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return activityDispatchingAndroidInjector
+    }
+
+
+}
